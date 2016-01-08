0a41034cf5e8a52a831492b7e04815fe656e6704
lkjy <1443799603@qq.com>
2016-01-08 16:47:30 +0800

    第一次提交

---
+++ SecondProgram/SecondProgram/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SecondProgram
+//
+//  Created by lkjy on 16/1/8.
+//  Copyright © 2016年 吴文成. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
